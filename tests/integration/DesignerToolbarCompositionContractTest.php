cdb0b8346752c14194455962ca87fdc3d46c6d39
Christiaan Bruinsma <236026742+christiaanbruinsma@users.noreply.github.com>
2026-09-13 19:51:14 +0200

    Designer Foundation: guard toolbar control geometry

--- tests/integration/DesignerToolbarCompositionContractTest.php
+++ tests/integration/DesignerToolbarCompositionContractTest.php
@@ -9,16 +9,22 @@ final class CB_Designer_Toolbar_Composition_Contract_Test extends WP_UnitTestCas
 		return (string) file_get_contents( dirname( __DIR__, 2 ) . '/' . ltrim( $path, '/' ) );
 	}
 
-	public function test_designer_mode_enqueues_the_base_owned_adaptive_toolbar_runtime(): void {
+	public function test_designer_mode_enqueues_the_base_owned_adaptive_toolbar_runtime_and_presentation(): void {
 		DesignEditorAssets::enqueue_designer_mode( 'Toolbar proof' );
 
 		self::assertTrue( wp_script_is( DesignEditorAssets::DESIGNER_MODE_SCRIPT, 'enqueued' ) );
 		self::assertTrue( wp_script_is( DesignEditorAssets::DESIGNER_TOOLBAR_SCRIPT, 'enqueued' ) );
+		self::assertTrue( wp_style_is( DesignEditorAssets::DESIGNER_TOOLBAR_STYLE, 'enqueued' ) );
 
 		$scripts = wp_scripts();
 		$toolbar = $scripts->registered[ DesignEditorAssets::DESIGNER_TOOLBAR_SCRIPT ] ?? null;
 		self::assertInstanceOf( _WP_Dependency::class, $toolbar );
 		self::assertContains( DesignEditorAssets::DESIGNER_MODE_SCRIPT, $toolbar->deps );
+
+		$styles        = wp_styles();
+		$toolbar_style = $styles->registered[ DesignEditorAssets::DESIGNER_TOOLBAR_STYLE ] ?? null;
+		self::assertInstanceOf( _WP_Dependency::class, $toolbar_style );
+		self::assertContains( DesignEditorAssets::DESIGNER_COMPOSITION_STYLE, $toolbar_style->deps );
 	}
 
 	public function test_compact_toolbar_is_capability_driven_and_keeps_primary_save_pinned(): void {
@@ -57,15 +63,28 @@ final class CB_Designer_Toolbar_Composition_Contract_Test extends WP_UnitTestCas
 		self::assertStringNotContainsString( 'aria-haspopup', $runtime );
 	}
 
+	public function test_toolbar_controls_share_one_canonical_geometry_and_compact_save_does_not_look_larger(): void {
+		$toolbar_css = $this->source( 'assets/css/design/designer-toolbar.css' );
+
+		self::assertStringContainsString( '--cb-design-toolbar-control-size: calc(var(--cb-control-height) + var(--cb-space-1));', $toolbar_css );
+		self::assertStringContainsString( 'inline-size: var(--cb-design-toolbar-control-size) !important;', $toolbar_css );
+		self::assertStringContainsString( 'block-size: var(--cb-design-toolbar-control-size) !important;', $toolbar_css );
+		self::assertStringContainsString( 'align-self: center;', $toolbar_css );
+		self::assertStringContainsString( '.cb-core-design-shell__compact-menu', $toolbar_css );
+		self::assertStringContainsString( '[data-cb-design-shell-primary-action].cb-core-button--primary', $toolbar_css );
+		self::assertStringContainsString( 'box-shadow: inset 0 0 0 1px currentColor !important;', $toolbar_css );
+	}
+
 	public function test_compact_toolbar_presentation_is_base_owned_and_not_mail_owned(): void {
-		$css      = $this->source( 'assets/css/design/designer-mode.css' );
-		$mail_css = $this->source( 'assets/css/pages/mail-designer.css' );
-		$icons    = $this->source( 'assets/js/design/shell/icons.js' );
+		$css         = $this->source( 'assets/css/design/designer-mode.css' );
+		$toolbar_css = $this->source( 'assets/css/design/designer-toolbar.css' );
+		$mail_css    = $this->source( 'assets/css/pages/mail-designer.css' );
+		$icons       = $this->source( 'assets/js/design/shell/icons.js' );
 
 		self::assertStringContainsString( '.cb-core-design-shell__toolbar--designer.is-compact', $css );
 		self::assertStringContainsString( '.cb-core-design-shell__compact-menu-popover', $css );
 		self::assertStringContainsString( '.cb-core-design-shell__compact-menu-item', $css );
-		self::assertStringContainsString( '[data-cb-design-shell-compact-group]', $css );
+		self::assertStringContainsString( '.cb-core-design-shell__toolbar--designer', $toolbar_css );
 		self::assertStringContainsString( 'ellipsis: Object.freeze([', $icons );
 		self::assertStringNotContainsString( 'cb-core-design-shell__compact-menu', $mail_css );
 	}
