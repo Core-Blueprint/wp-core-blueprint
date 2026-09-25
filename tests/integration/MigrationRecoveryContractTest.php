41d9255fbba3ed10f1d8b10fcfc14bd0171a6cdf
Christiaan Bruinsma <236026742+christiaanbruinsma@users.noreply.github.com>
2026-09-26 00:44:36 +0200

    Use canonical settings mutation in recovery tests

--- tests/integration/MigrationRecoveryContractTest.php
+++ tests/integration/MigrationRecoveryContractTest.php
@@ -251,7 +251,7 @@ final class MigrationRecoveryContractTest extends WP_UnitTestCase {
 
 		$settings = CB\Core\Settings::get();
 		$settings['two_factor'] = [ 'mode' => TwoFactorPolicy::MODE_ENFORCE, 'scope' => TwoFactorPolicy::SCOPE_PRIVILEGED ];
-		CB\Core\Settings::update( $settings );
+		CB\Core\Settings::set_key( 'two_factor', $settings['two_factor'], 'migration_recovery_test' );
 
 		$ticket = (string) Recovery::issue_ticket( 'migration-two-factor-enforce', 'https://destination.test' )['ticket'];
 		CredentialStore::store_totp_secret( $actor_id, 'JBSWY3DPEHPK3PXP' );
